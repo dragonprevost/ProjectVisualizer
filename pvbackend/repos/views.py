d505217a27812d74c605f1ef84003c5a793c7f9d
dragonprevost <dragon@dkp.io>
2018-11-22 09:37:55 -0800

    added repo viewset

--- pvbackend/repos/views.py
+++ pvbackend/repos/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from django.contrib.auth.models import User
+from pvbackend.repos.serializers import RepoSerializer
 
-# Create your views here.
+class RepoViewSet(viewsets.ModelViewSet):
+
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = RepoSerializer
+
+
+
