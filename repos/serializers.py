b814429e44254a83ac450b2837e71936247cab8a
dragonprevost <dragon@dkp.io>
2018-11-22 13:36:09 -0800

    created serializer for repo

---
+++ repos/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+from repos.models import Repo
+
+
+class SnippetSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    title = serializers.CharField(required=False, allow_blank=True, max_length=100)
+    url = serializers.ChoiceField(choices=LANGUAGE_CHOICES, default='python')
+
+    def create(self, validated_data):
+        """
+        Create and return a new `Snippet` instance, given the validated data.
+        """
+        return Snippet.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        """
+        Update and return an existing `Snippet` instance, given the validated data.
+        """
+        instance.title = validated_data.get('title', instance.title)
+        instance.url = validated_data.get('language', instance.language)
+        instance.save()
+        return instance
