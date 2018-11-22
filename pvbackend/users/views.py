9b6151acb7c1a1c03b4914de59b42b8752549fc5
dragonprevost <dragon@dkp.io>
2018-11-22 13:27:33 -0800

    removed old repos app

--- pvbackend/users/views.py
+++ pvbackend/users/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from django.contrib.auth.models import User
-from pvbackend.repos.serializers import RepoSerializer
+from pvbackend.users.serializers import UserSerializer
 
 class UserViewSet(viewsets.ModelViewSet):
 
     queryset = User.objects.all().order_by('-date_joined')
-    serializer_class = RepoSerializer
+    serializer_class = UserSerializer
 
 
