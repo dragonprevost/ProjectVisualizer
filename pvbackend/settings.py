4e352213043c569356913bf152cf463a83ba4a04
dragonprevost <dragon@dkp.io>
2018-11-21 18:56:57 -0800

    updated db setting

--- pvbackend/settings.py
+++ pvbackend/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'pvbackend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'pvdb',
+        'USER': 'django',
+        'PASSWORD': 'password123',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
