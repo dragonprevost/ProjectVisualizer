07628bb656f28b2ad4160e7990608a0ce8be61e6
dragonprevost <dragon@dkp.io>
2018-11-22 13:48:32 -0800

    imports added to repo views

--- repos/views.py
+++ repos/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
 
-# Create your views here.
+from repos.models import Repo
+from repos.serializers import RepoSerializer
+
+
