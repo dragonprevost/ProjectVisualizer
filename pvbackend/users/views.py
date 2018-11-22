29ced3172d883d1e80c071c2d464988ac0a829e1
dragonprevost <dragon@dkp.io>
2018-11-22 10:45:08 -0800

    added users application

---
+++ pvbackend/users/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from django.contrib.auth.models import User
+from pvbackend.repos.serializers import RepoSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = RepoSerializer
+
+
